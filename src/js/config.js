7cca3468c9cde46dd111713295963bb7610120fc
ursanor <89756520+OnurUrsar@users.noreply.github.com>
2023-04-17 20:31:52 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "92c3eee9-925a-413e-bbc5-8a2e82bb26bc";
+export const MODAL_CLOSE_SEC = 2.5;
