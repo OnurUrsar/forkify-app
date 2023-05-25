6b2d2c4baf0930aca8ca8ddf574421b3549bb941
ursanor <89756520+OnurUrsar@users.noreply.github.com>
2023-05-25 14:25:28 +0300

    API key change

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = "92c3eee9-925a-413e-bbc5-8a2e82bb26bc";
+export const KEY = "c8477e59-4458-4181-afd8-8289a083acfb";
 export const MODAL_CLOSE_SEC = 2.5;
